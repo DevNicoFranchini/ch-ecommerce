9a2199e71057fc962887ef81514584dd9801f98e
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-23 12:03:09 -0300

    [Add] db configuration

---
+++ server/src/config/db.config.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
+import { options } from './options.config.js';
+
+// DB CONNECTION
+
+class UserDB {
+	constructor(url) {
+		this.url = url;
+	}
+
+	async connect() {
+		try {
+			await mongoose.connect(this.url, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			});
+			console.log('Conexión a la base de datos de manera exitosa');
+		} catch (error) {
+			console.error(`Hubo un error conectándose a la base. El error es: ${error}`);
+		}
+	}
+
+	async disconnect() {
+		try {
+			await mongoose.disconnect();
+			console.log('Desconexión de la base de datos de manera exitosa');
+		} catch (error) {
+			console.error(`Hubo un error desconectándose de la base. El error es: ${error}`);
+		}
+	}
+}
+
+// SESSION CONFIG
+
+const sessionDB = (app) => {
+	app.use(
+		session({
+			store: MongoStore.create({
+				mongoUrl: options.mongodb.mongosessions,
+				mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+                // collectionName: 'test',
+			}),
+			secret: 'claveSecreta',
+			resave: false,
+			saveUninitialized: true,
+		})
+	);
+};
+
+export { UserDB, sessionDB };
