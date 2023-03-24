2bbfb8db2e15cd54905966afc364b19777833d11
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-24 15:37:39 -0300

    [Update] Index routes

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -4,7 +4,7 @@ import { UserRouter } from './api/user.routes.js';
 import { ProductsRouter } from './api/products.routes.js';
 import { CartRouter } from './api/cart.routes.js';
 
-// import * as UserController from './../controllers/users.controller.js';
+import * as UserController from './../controllers/user.controller.js';
 
 const router = express.Router();
 
@@ -28,7 +28,7 @@ router.use('/products', ProductsRouter);
 router.use('/cart', CartRouter);
 
 // Desafio 20
-// router.get('/all-users', UserController.getUsersController);
-// router.post('/new-user', UserController.saveUserController);
+router.get('/all-users', UserController.getUsersController);
+router.post('/new-user', UserController.saveUserController);
 
 export { router as apiRouter };
