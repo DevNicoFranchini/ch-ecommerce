1777b46f5d3c059cdf138542d3861f473764ca82
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-24 19:32:29 -0300

    [Add] Views

---
+++ server/src/public/index.js
@@ -0,0 +1,12 @@
+console.log('JS Funcionando');
+
+const inputUsername = document.getElementById('inputUsername');
+const inputName = document.getElementById('inputName');
+const inputLastName = document.getElementById('inputLastName');
+const inputAge = document.getElementById('inputAge');
+const inputAlias = document.getElementById('inputAlias');
+const inputAvatar = document.getElementById('inputAvatar');
+const inputMensaje = document.getElementById('inputMensaje');
+const btnEnviar = document.getElementById('btnEnviar');
+const compresion = document.getElementById('compresion');
+const formPublicarMensaje = document.getElementById('formPublicarMensaje');
