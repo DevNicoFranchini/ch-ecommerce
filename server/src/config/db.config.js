12ad73b21248ff76a7a6f89ef36db4d9ca2a379f
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-24 19:24:58 -0300

    [Update] db config

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,39 +1,7 @@
-import mongoose from 'mongoose';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
 
 import { options } from './options.config.js';
-import { logger } from './../logs/logger.js';
-
-// DB CONNECTION
-
-class UserDB {
-	constructor(url, dbname) {
-		this.url = url;
-		this.dbname = dbname;
-	}
-
-	async connect() {
-		try {
-			await mongoose.connect(this.url, {
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			});
-			logger.info(`CONEXIÓN A LA BASE DE DATOS "${this.dbname}" DE MANERA EXITOSA`);
-		} catch (error) {
-			logger.warn(`HUBO UN ERROR CONECTÁNDOSE A LA BASE. EL ERROR ES: ${error}`);
-		}
-	}
-
-	async disconnect() {
-		try {
-			await mongoose.disconnect();
-			logger.info('DESCONEXIÓN DE LA BASE DE DATOS DE MANERA EXITOSA');
-		} catch (error) {
-			logger.warn(`HUBO UN ERROR DESCONECTÁNDOSE DE LA BASE. EL ERROR ES: ${error}`);
-		}
-	}
-}
 
 // SESSION CONFIG
 
@@ -41,9 +9,8 @@ const sessionDB = (app) => {
 	app.use(
 		session({
 			store: MongoStore.create({
-				mongoUrl: options.mongodb.mongosessions,
+				mongoUrl: options.mongodb.mongourl,
 				mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
-				// collectionName: 'test',
 			}),
 			secret: 'claveSecreta',
 			resave: false,
@@ -52,4 +19,4 @@ const sessionDB = (app) => {
 	);
 };
 
-export { UserDB, sessionDB };
+export { sessionDB };
