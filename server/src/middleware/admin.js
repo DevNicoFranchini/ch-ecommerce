d1e7f8dd69419b1653534d9ea8ec6b0a8719faa6
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-25 09:19:23 -0300

    [Update] Add render

--- server/src/middleware/admin.js
+++ server/src/middleware/admin.js
@@ -5,14 +5,7 @@ const admin = options.server.admin;
 const isAdmin = (req, res, next) => {
 	const username = req.session.passport.user.username;
 	if (username != admin) {
-		res.status(403).json({
-			error: -1,
-			description: {
-				route: req.path,
-				method: req.method,
-				msg: 'Unauthorized',
-			},
-		});
+		res.status(403).render('unauthorized');
 	} else {
 		next();
 	}
