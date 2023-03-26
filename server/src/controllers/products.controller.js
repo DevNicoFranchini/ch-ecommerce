4857a1c69ba3c3256d66e8d2b1d9783eb384ab20
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-26 13:41:20 -0300

    [Create] Products controller

---
+++ server/src/controllers/products.controller.js
@@ -0,0 +1,37 @@
+import {
+	saveProduct,
+	getProducts,
+	getProductsById,
+	updateProductById,
+	deleteProducts,
+	deleteProductsById,
+} from './../models/services/product.service.js';
+
+export const getProductsController = async (req, res) => {
+	try {
+		const username = req.session.passport.user.username
+		const products = await getProducts();
+		res.status(200).render('products', { products, username });
+	} catch (error) {
+		res.status(400).json({ message: `HUBO UN ERROR. EL ERROR FUE: ${error}` });
+	}
+};
+
+export const saveProductController = async (req, res) => {
+	try {
+		const product = await saveProduct(req.body);
+		res.status(200).json({ data: product });
+	} catch (error) {
+		res.status(400).json({ message: `HUBO UN ERROR. EL ERROR FUE: ${error}` });
+	}
+};
+
+export const updateProductController = async (req, res) => {
+	try {
+		const product = await updateById(req.body, req.params.id);
+		const products = await getProducts();
+		res.status(200).render('users', { products });
+	} catch (error) {
+		res.status(400).json({ message: `HUBO UN ERROR. EL ERROR FUE: ${error}` });
+	}
+};
