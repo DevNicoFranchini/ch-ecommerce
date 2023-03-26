a5b044a040a5a1892c2b418e4bf8a98357fac301
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-26 13:42:26 -0300

    [Update] Complete service

--- server/src/models/services/product.service.js
+++ server/src/models/services/product.service.js
@@ -1,12 +1,28 @@
+import { getApiDao } from './../index.models.js';
 import { options } from './../../config/options.config.js';
-import { getApiDao } from './../persistence/models/index.js';
 
-const { UserDaoContainer } = await getApiDao(options.server.dbType);
+const { ProductDaoContainer } = await getApiDao(options.server.dbType);
 
-export const getUsers = async () => {
-	return await UserDaoContainer.getAll();
+export const saveProduct = async (body) => {
+	return await ProductDaoContainer.save(body);
 };
 
-export const saveUser = async (body) => {
-	return await UserDaoContainer.save(body);
+export const getProducts = async () => {
+	return await ProductDaoContainer.getAll();
+};
+
+export const getProductsById = async (id) => {
+	return await ProductDaoContainer.getById(id);
+};
+
+export const updateProductById = async (body, id) => {
+	return await ProductDaoContainer.updateById(body, id);
+};
+
+export const deleteProducts = async () => {
+	return await ProductDaoContainer.deleteAll();
+};
+
+export const deleteProductsById = async (id) => {
+	return await ProductDaoContainer.deleteById(id);
 };
