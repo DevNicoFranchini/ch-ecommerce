87a4414c1a03611319a3d0178e330ea649b0e141
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-26 13:44:07 -0300

    [Update] Redirect home

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -4,16 +4,13 @@ import { UserRouter } from './api/user.routes.js';
 import { ProductsRouter } from './api/products.routes.js';
 import { CartRouter } from './api/cart.routes.js';
 
-import * as UserController from './../controllers/user.controller.js';
-
 const router = express.Router();
 
 // GET
 
 router.get('/', (req, res) => {
 	if (req.isAuthenticated()) {
-		const username = req.session.passport.user.username;
-		res.render('home', { username });
+		res.redirect('/api/products');
 	} else {
 		res.redirect('/api/users/login');
 	}
