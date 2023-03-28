78179bcf6bfd224934567b09e6f99711248c673f
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-03-28 10:40:44 -0300

    [Update] /cart route

--- server/src/routes/api/cart.routes.js
+++ server/src/routes/api/cart.routes.js
@@ -7,7 +7,7 @@ import * as CartController from './../../controllers/cart.controller.js';
 const router = express.Router();
 
 // router.post('/new', CartController.saveCartController)
-router.post('/', CartController.existsCartController);
+router.get('/', CartController.existsCartController);
 router.get('/all-carts', isAdmin, CartController.getCartsController);
 
 export { router as CartRouter };
