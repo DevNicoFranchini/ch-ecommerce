8975234082bfbdd4c56c27136002efe958e2a684
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-04-02 00:18:33 -0300

    [Add] Route to add product by id

--- server/src/routes/api/cart.routes.js
+++ server/src/routes/api/cart.routes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.get('/', CartController.existsCartController);
 router.delete('/delete', CartController.deleteCartController);
+router.post('/add/:id', CartController.addProductCartController);
 router.get('/all-carts', isAdmin, CartController.getCartsController);
 
 export { router as CartRouter };
