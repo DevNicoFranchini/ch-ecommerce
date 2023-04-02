a259c011f715488ca4a6076c2488407b10b36a8b
Nicolas Franchini <devnicolasfranchini@gmail.com>
2023-04-02 00:24:02 -0300

    [Update] crud option for web use

--- server/src/routes/api/cart.routes.js
+++ server/src/routes/api/cart.routes.js
@@ -7,8 +7,8 @@ import * as CartController from './../../controllers/cart.controller.js';
 const router = express.Router();
 
 router.get('/', CartController.existsCartController);
-router.delete('/delete', CartController.deleteCartController);
-router.post('/add/:id', CartController.addProductCartController);
+router.get('/delete', CartController.deleteCartController);
+router.get('/add/:id', CartController.addProductCartController);
 router.get('/all-carts', isAdmin, CartController.getCartsController);
 
 export { router as CartRouter };
